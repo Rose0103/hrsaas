3e55dc2927623778080c541df2583512c6a0c71c
Rose0103 <1604857467@qq.com>
2022-02-10 09:36:03 +0800

    左侧路由

---
+++ src/router/modules/attendances.js
@@ -0,0 +1,19 @@
+// 导出员工的路由规则
+import layout from '@/layout'
+
+export default {
+  // 路由规则
+  path: '/attendances',
+  component: layout,
+  // 配置二级路由的路由表
+  children: [
+    {
+      path: '', // 这里当二级路由的path什么都不写的时候 表示该路由为当前二级路由的默认路由
+      component: () => import('@/views/attendances'),
+      // 路由元信息  其实就是存储数据的对象 我们可以在这里放置一些信息
+      meta: {
+        title: '考勤' // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+      }
+    }
+  ]
+}
