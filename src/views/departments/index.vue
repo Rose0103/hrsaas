004e2a235ad5f11f4b8c59493c749ae74668fa0b
Rose0103 <1604857467@qq.com>
2022-02-10 14:45:31 +0800

    组织架构列表查询和删除部门

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,52 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织
-      </h2>
+      <!-- 组织架构内容 头部 -->
+      <el-card class="tree-card">
+        <TreeTools :tree-node="company" :is-root=" true " />
+        <!-- 树形菜单 -->
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true" @delDepts="getDepartments">
+          <TreeTools slot-scope="{ data }" :tree-node="data" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils'
 export default {
-
+  components: {
+    TreeTools
+  },
+  data() {
+    return {
+      company: {},
+      departs: [],
+      defaultProps: {
+        label: 'label'
+      }
+    }
+  },
+  created() {
+    this.getDepartments()
+  },
+  methods: {
+    async getDepartments() {
+      const result = await getDepartments()
+      this.company = { name: result.companyName, manager: '负责人' }
+      this.departs = tranListToTreeData(result.depts, '') // 需要将其转换成树形结构
+      console.log(result)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
