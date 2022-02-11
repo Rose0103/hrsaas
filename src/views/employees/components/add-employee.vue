d9f3bcaa832830d78e3abaf3c83d0c5333f93b49
Rose0103 <1604857467@qq.com>
2022-02-11 16:33:56 +0800

    点击部门赋值表单数据

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -12,13 +12,17 @@
         <el-date-picker v-model="employeeForm.timeOfEntry" style="width:50%" placeholder="请选择入职时间" />
       </el-form-item>
       <el-form-item label="聘用形式" prop="formOfEmployment">
-        <el-select v-model="employeeForm.formOfEmployment" style="width:50%" placeholder="请选择" />
+        <el-select v-model="employeeForm.formOfEmployment" style="width:50%" placeholder="请选择">
+          <el-option v-for="item in EmployeeEnum.hireType" :key="item.id" :label="item.value" :value="item.id" />
+        </el-select>
       </el-form-item>
       <el-form-item label="工号" prop="workNumber">
         <el-input v-model="employeeForm.workNumber" style="width:50%" placeholder="请输入工号" />
       </el-form-item>
       <el-form-item label="部门" prop="departmentName">
-        <el-input v-model="employeeForm.departmentName" style="width:50%" placeholder="请选择部门" />
+        <el-input v-model="employeeForm.departmentName" style="width:50%" placeholder="请选择部门" @focus="getDepartments" />
+        <!-- 放置一个tree -->
+        <el-tree v-if="showTree" v-loading="loading" :data="treeData" :props="defaultProps" :default-expand-all="true" @node-click="selectNode" />
       </el-form-item>
       <el-form-item label="转正时间" prop="correctionTime">
         <el-date-picker v-model="employeeForm.correctionTime" style="width:50%" placeholder="请选择转正时间" />
@@ -37,6 +41,9 @@
 </template>
 
 <script>
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils'
+import EmployeeEnum from '@/api/constant/employees'
 export default {
   props: {
     showDialog: {
@@ -46,8 +53,16 @@ export default {
   },
   data() {
     return {
+      EmployeeEnum,
       // 定义表单数据
-      employeeForm: {},
+      employeeForm: {
+        username: '',
+        mobile: '',
+        formOfEmployment: '',
+        workNumber: '',
+        departmentName: '',
+        timeOfEntry: ''
+      },
       rules: {
         username: [{ required: true, message: '用户姓名不能为空', trigger: 'blur' }, {
           min: 1, max: 4, message: '用户姓名为1-4位', trigger: 'blur'
@@ -59,7 +74,29 @@ export default {
         workNumber: [{ required: true, message: '工号不能为空', trigger: 'blur' }],
         departmentName: [{ required: true, message: '部门不能为空', trigger: 'change' }],
         timeOfEntry: [{ required: true, message: '入职时间', trigger: 'blur' }]
-      }
+      },
+      treeData: [], // 定义一个数组来接收树形结构
+      defaultProps: {
+        label: 'name'
+      },
+      showTree: false, // tree默认不显示
+      loading: false // 控制树的显示或者隐藏进度条
+    }
+  },
+  methods: {
+    // 部门
+    async getDepartments() {
+      this.showTree = true
+      this.loading = true
+      const { depts } = await getDepartments()
+      // depts是一个数组，需要转换成树形结构
+      this.treeData = tranListToTreeData(depts, '')
+      this.loading = false
+    },
+    // **选择部门，赋值表单数据**
+    selectNode(node) {
+      this.employeeForm.departmentName = node.name
+      this.showTree = false
     }
   }
 }
